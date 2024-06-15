a2291039ee01e8679b1cde00c587c856143db5c9
Ankit Dabhi <ankitdabhi@192.168.1.7>
2024-06-15 13:24:51 +0530

    inital commit

---
+++ src/com/java/leetcode/hundered_liked/easy/LongestCommonPrefix.java
@@ -0,0 +1,12 @@
+package com.java.leetcode.hundered_liked.easy;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    private static String logestCommonPrefix(String[] value) {
+        return null;
+    }
+
+}
